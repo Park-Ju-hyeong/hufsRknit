eeb31ac5fff3b309fca6327eb40262d68b9f492a
= <=>
2017-06-29 14:43:53 +0900

    폰트 적용 오류

---
+++ js/webcafe.js
@@ -0,0 +1,22 @@
+$(function() {
+    //if 구문처럼
+    $(".main-menu>li").hover(
+        //True
+        function() {
+            $(this).children("ul").css("display", "block");
+        },
+        //False
+        function() {
+            $(this).children("ul").css("display", "none")
+        });
+    $(".main-menu>li>span").focusin(
+        //True
+        function() {
+            $(this).siblings("ul").css("display", "block");
+        });
+    $(".main-menu>li>ul>li:last-child a").focusout(
+        //True
+        function() {
+            $(this).parent().parent().css("display", "none");
+        });
+});
